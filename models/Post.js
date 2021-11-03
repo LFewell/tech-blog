34c076d72f95432223c9e90fdc6cd827f06dff17
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-11-03 01:40:45 -0400

    Integrated project code

--- models/Post.js
+++ models/Post.js
@@ -4,13 +4,40 @@ const sequelize = require("../config/connection");
 class Post extends Model {}
 
 Post.init(
-    {
-        title: DataTypes.STRING,
-        body: DataTypes.STRING
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
 );
 
-module.exports = Post;
+module.exports = Blog;
